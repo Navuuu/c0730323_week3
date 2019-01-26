f3f3a51f0d2bc20513d3799544967ec9c917367f
ravneet kaur <kaurravneet.btl1216@gmail.com>
2019-01-25 21:16:42 -0500

    last commit

--- c0730323/Program.cs
+++ c0730323/Program.cs
@@ -16,14 +16,42 @@ namespace c0730323
         }
         class Countryside
         {
+            
+            village Alst;
+            village Maeland;
+            village schenig;
+                public void initializemap()
+            { 
+                 Alst = new village();
+                Alst.VillageName = "Alst";
+                 Maeland = new village();
+                Maeland.VillageName = " Maeland";
+                Maeland.isAstrildeHere = true;
+                schenig = new village();
+                schenig.VillageName = "schenig";
 
+                Alst.Nextvillage = Maeland;
+                Maeland.Nextvillage = schenig;
+                schenig.Nextvillage = Alst;
+
+            }
+            public void FindAstrilde()
+            {
+                 village CurrentVillage = this.Maeland;
+                if (CurrentVillage.isAstrildeHere)
+                {
+                    Console.WriteLine("hugi Found astrilde!!! Celebrations! XOXO");
+                }
+                village NextVillage;
+                NextVillage = CurrentVillage.Nextvillage;
+            }
         }
         class village
         {
-            village Previousvillage;
-            village Nextvillage;
-            string VillageName;
-            bool isAstrildeHere = false;
+            public village Previousvillage;
+            public village Nextvillage;
+            public string VillageName;
+            public bool isAstrildeHere = false;
 
             public village() { }
         }
